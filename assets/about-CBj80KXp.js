bab12d028c6e2762bd72549eced0b291a5b382cd
Minghua <sornmai148@gmail.com>
2026-01-08 17:01:15 +0700

    Updates

---
+++ assets/about-CBj80KXp.js
@@ -0,0 +1,1 @@
+import{u as n,j as t}from"./index-B4SpxMjH.js";function e(){const{t:s}=n();return t.jsx("div",{className:"p-2 text-red-500",children:t.jsx("p",{children:s("about")})})}export{e as component};
