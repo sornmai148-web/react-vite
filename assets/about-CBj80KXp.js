5c7327555d4e8edfb8c7785c867df0ba25adbabe
Minghua <sornmai148@gmail.com>
2026-01-09 15:30:36 +0700

    Updates

--- assets/about-CBj80KXp.js
+++
@@ -1,1 +0,0 @@
-import{u as n,j as t}from"./index-B4SpxMjH.js";function e(){const{t:s}=n();return t.jsx("div",{className:"p-2 text-red-500",children:t.jsx("p",{children:s("about")})})}export{e as component};
