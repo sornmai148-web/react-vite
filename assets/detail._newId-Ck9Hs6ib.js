616f931f6cf0162db23a2f2f80cd4ae7ce5d339a
Minghua <sornmai148@gmail.com>
2026-01-10 10:21:14 +0700

    Updates

--- assets/detail._newId-Ck9Hs6ib.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BoCDKKGq.js";function n(){return o.jsx("div",{children:'Hello "/detail/$newId"!'})}export{n as component};
---
+++ assets/detail._newId-RMghZ5ri.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DG2NTfqa.js";function n(){return o.jsx("div",{children:'Hello "/detail/$newId"!'})}export{n as component};
