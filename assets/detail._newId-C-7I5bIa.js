33b4d3aed760f5cc1e22244c9f35a5856f5b6425
Minghua <sornmai148@gmail.com>
2026-01-12 14:31:29 +0700

    Updates

--- assets/detail._newId-C-7I5bIa.js
+++
@@ -1,1 +0,0 @@
-import{q as o}from"./index-B5IYdOs8.js";function n(){return o.jsx("div",{children:'Hello "/detail/$newId"!'})}export{n as component};
--- assets/react-qa0xXszB.js
+++ assets/react-qa0xXszB.js
@@ -1,4 +1,4 @@
-import{R as O}from"./index-B5IYdOs8.js";import{M as P,C as A}from"./mixin-D3UDm--6.js";import{H as h}from"./hls-CiLLo4G9.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
+import{R as O}from"./index-D1dSsVDL.js";import{M as P,C as A}from"./mixin-D3UDm--6.js";import{H as h}from"./hls-CiLLo4G9.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
         <script type="application/json" id="config">
           ${JSON.stringify(s.config||{})}
         <\/script>
