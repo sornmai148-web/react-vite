8b90574ed8dab4886b8520443c8fa737f45953c2
Minghua <sornmai148@gmail.com>
2026-01-12 16:34:01 +0700

    Updates

--- assets/react-CI_DHlS9.js
+++ assets/react-CI_DHlS9.js
@@ -1,4 +1,4 @@
-import{R as O}from"./index-uqrNlel6.js";import{M as P,C as A}from"./mixin-D3UDm--6.js";import{H as h}from"./hls-CiLLo4G9.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
+import{R as O}from"./index-Bwbmv_Qf.js";import{M as P,C as A}from"./mixin-D3UDm--6.js";import{H as h}from"./hls-CiLLo4G9.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
         <script type="application/json" id="config">
           ${JSON.stringify(s.config||{})}
         <\/script>
