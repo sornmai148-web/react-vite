9ea8f5546f30e185986764e3cfa88023e991e328
Minghua <sornmai148@gmail.com>
2026-01-12 14:04:12 +0700

    Updates

--- assets/detail._newId-Bi07G2vP.js
+++
@@ -1,1 +0,0 @@
-import{q as o}from"./index-DnHnfQJD.js";function n(){return o.jsx("div",{children:'Hello "/detail/$newId"!'})}export{n as component};
---
+++ assets/detail._newId-C-7I5bIa.js
@@ -0,0 +1,1 @@
+import{q as o}from"./index-B5IYdOs8.js";function n(){return o.jsx("div",{children:'Hello "/detail/$newId"!'})}export{n as component};
